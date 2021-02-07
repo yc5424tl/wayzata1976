762804ce3cda2168a7ba49f7f62ae5e5718f2071
Jacob Boline <17325414+yc5424tl@users.noreply.github.com>
2021-02-06 18:46:33 -0600

    Update models.py

--- wayzata1976_app/wayzata76_web/models.py
+++ wayzata1976_app/wayzata76_web/models.py
@@ -104,6 +104,9 @@ def gallery_for_image(instance, filename):
     print(f'type(instance) = {type(instance)}')
     print(f'filename = {filename}')
     print(f'instance = {instance}')
+    print(f'instance.gallery = {instance.gallery}')
+    print(f'instance.gallery.working_name = {instance.gallery.working_name}')
+    print(f'instance.uuid & ext = {instance.uuid} , ext = {ext}')
     name, ext = filename.replace(' ', '_').split(".")
     file_path = f"{instance.gallery.working_name}/{instance.uuid}.{ext}"
     return file_path
