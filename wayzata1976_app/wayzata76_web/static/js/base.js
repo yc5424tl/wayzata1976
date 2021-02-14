887365b7e05904f397ca9edeb9c1ee2c1dee8b10
“jacob.boline” <jacob.boline.mn@gmail.com>
2021-02-14 05:57:42 -0600

    nav fin

--- wayzata1976_app/wayzata76_web/static/js/base.js
+++ wayzata1976_app/wayzata76_web/static/js/base.js
@@ -1,73 +1,19 @@
-// $(document).ready(function() {
-
-//     // Prevent closing from click inside dropdown
-//     $(document).on('click', '.dropdown-menu', function (e) {
-//       e.stopPropagation();
-//     });
-
-//     // $(document).on('tap', '.dropdown-menu', function (e) {
-//     //   e.stopPropagation();
-//     // });
-
-//     // make it as accordion for smaller screens
-//     if ($(window).width() < 992) {
-
-//       $('.dropdown-menu a').click(function(e){
-//         e.preventDefault();
-//           if ($(this).next('.submenu').length){
-//             $(this).next('.submenu').toggle();
-//           }
-//           $('.dropdown').on('hide.bs.dropdown', function () {
-//         $(this).find('.submenu').hide();
-//       })
-//       });
-
-
-//       // $('.dropdown-menu a').on('tap', function(e){
-//       //   e.preventDefault();
-//       //     if ($(this).next('.submenu').length){
-//       //       $(this).next('.submenu').toggle();
-//       //     }
-//       //     $('.dropdown').on('hide.bs.dropdown', function() {
-//       //   $(this).find('.submenu').hide();
-//       // })
-//       // });
-
-
-//     }
-
-
-
-
-  $(document).on('click', '.dropdown-menu', function(e) {
-    e.preventDefault();
-    $(this).next('.submenu').toggle();
-    $('.dropdown').on('hide.bs.dropdown', function () {
-      $(this).find('.submenu').hide();
+$(document).ready(function() {
+  $(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
   });
+  
+  // make it as accordion for smaller screens
+  if ($(window).width() < 992) {
+    $('.dropdown-menu a').click(function(e){
+      e.preventDefault();
+        if($(this).next('.submenu').length){
+          $(this).next('.submenu').toggle();
+        }
+        $('.dropdown').on('hide.bs.dropdown', function () {
+       $(this).find('.submenu').hide();
+    })
+    });
+  }
+})
 
-
-    // window.onclick = function(event) {
-    //   if (event.target.matches('.pic-nav')) {
-    //     event.stopPropagation();
-    //   }
-    // }
-
-    // $(document).on('click', '.allow-focus #nav-pics-dropdown', function (e) {
-    //     e.stopPropagation();
-    //   });
-
-    //   // Collapse accordion every time dropdown is shown
-    // $('.dropdown-accordion').on('show.bs.dropdown', function (event) {
-    //   var accordion = $(this).find($(this).data('accordion'));
-    //   accordion.find('.panel-collapse.in').collapse('hide');
-    // });
-
-    // // Prevent dropdown to be closed when we click on an accordion link
-    // $('.dropdown-accordion').on('click', 'a[data-toggle="collapse"]', function (event) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    //   $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
-    //   $($(this).attr('href')).collapse('show');
-    // });
-// });
--- wayzata1976_app/wayzata76_web/static/js/photos/multi-upload.js
+++ wayzata1976_app/wayzata76_web/static/js/photos/multi-upload.js
@@ -3,17 +3,6 @@ $(function () {
         $("#fileupload").click();
     });
 
-    // $("#fileupload").fileupload({
-    //     dataType: 'json',
-    //     done: function (e, data) {
-    //         if (data.result.is_valid) {
-    //             $("#gallery tbody").prepend(
-    //                 "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
-    //             )
-    //         }
-    //     }
-    // });
-
 
     $("#fileupload").fileupload({
         dataType: 'json',
