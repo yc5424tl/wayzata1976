ff3a65d6dc4571cf13fce3c76a1fc13e34bdd080
Jacob Boline <17325414+yc5424tl@users.noreply.github.com>
2023-04-15 11:45:08 -0500

    Update urls.py

--- wayzata1976_app/wayzata76_django/urls.py
+++ wayzata1976_app/wayzata76_django/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 import os
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('wayzata76_web.urls')),
     path('tinymce/', include('tinymce.urls')),
